8ff6d48b195a786e00c1952189841a6be80b75e8
Souza <joao.souza@capgemini.com>
2022-08-26 14:08:51 -0300

    Comentando contextLoads

--- src/test/java/com/application/OpenShoesJavaApplicationTests.java
+++ src/test/java/com/application/OpenShoesJavaApplicationTests.java
@@ -1,13 +1,13 @@
 package com.application;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OpenShoesJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
